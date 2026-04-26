d5fc023b1686803079415f0b062287cf1cd6dd7c
Inno-prog <inno.prog@example.com>
2026-04-26 14:01:31 +0000

    mobile3

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,17 @@ import { Component, AfterViewInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './components/navbar/navbar';
 import { Footer } from './components/footer/footer';
+import { Hero } from './components/hero/hero';
+import { About } from './components/about/about';
+import { Projects } from './components/projects/projects';
+import { Experience } from './components/experience/experience';
+import { Contact } from './components/contact/contact';
+import { Services } from './components/services/services';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Navbar, Footer],
+  imports: [RouterOutlet, Navbar, Footer, Hero, About, Projects, Experience, Contact, Services],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
